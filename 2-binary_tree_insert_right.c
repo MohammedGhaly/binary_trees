377581f03bd584566f5040b62b6e1a1686c39507
MohammedGhaly <medooghaly7@gmail.com>
2023-08-01 17:49:47 -0700

    tasks [2, 3] solved

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -6,12 +6,13 @@
  * @value: value carried by the node
  * Return: pointer to the new node
  */
-binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
+binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
-	binary_tree_t *new = binary_tree_node(parent, value);
+	binary_tree_t *new;
 
 	if (!parent)
 		return (NULL);
+	new = binary_tree_node(parent, value);
 	if (!new)
 		return (NULL);
 
