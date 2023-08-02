229df9f4eb9fd24feb69785d497f702a4d626073
MohammedGhaly <medooghaly7@gmail.com>
2023-08-02 10:41:02 -0700

    task 12 fixed

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -13,3 +13,18 @@ size_t binary_tree_leaves(const binary_tree_t *tree)
 		return (1);
 	return (binary_tree_leaves(tree->left) + binary_tree_leaves(tree->right));
 }
+
+/**
+ * binary_tree_is_leaf - checks if the node passed is a leaf node
+ * @node: root node of the tree
+ * Return: 1 if node is a leaf, 0 otherwise
+ */
+int binary_tree_is_leaf(const binary_tree_t *node)
+{
+	if (!node)
+		return (0);
+
+	if (node->left == NULL && node->right == NULL)
+		return (1);
+	return (0);
+}
